d0d8fb3f050e2ac7b63bcfe62757be560d45e5a3
Hagen Wey <Hagen.Wey@Student.HTW-Berlin.de>
2022-05-05 21:47:04 +0200

    css coding for login page
    ui coding for the two login types

--- loginPage/login.js
+++ loginPage/login.js
@@ -5,10 +5,22 @@
 var attemptOnAdminLogin = 3;
 var attemptOnStaffLogin = 3;
 
+/**
+ * This function validates th eadmin login data ONLY. It reads the input 
+ * via .getElementById().value and compairs it to the static given login data. 
+ * If the data is not exactly what it should be, the method decrements the attemps
+ * and sends an allert to the user. After three attmeps the input fields get 
+ * disabled as well as the Button. 
+ *   
+ * @returns true, if the login data is correct and false, if it`s not
+ */
 function validateAdminLogin()
-{   
+{  
+    // reading the input of the login data  
     var username = document.getElementById("adminUsername").value;
     var password = document.getElementById("adminPassword").value;
+    
+    // compiaring the login data 
     if(username == "Admin" && password == "Admin")
     {   
         return true;
@@ -18,6 +30,7 @@ function validateAdminLogin()
         //decrementing attempt by one
         attemptOnAdminLogin--;
 
+        //just a small information about the failure and the left attemps
         alert("Login Credentials Incorrect\n" + "You have left " + attemptOnAdminLogin + " attempts!");
         if(attemptOnAdminLogin == 0)
         {
@@ -30,10 +43,20 @@ function validateAdminLogin()
     }   
 }
 
+/**
+ * This function validates the staff login data ONLY. It reads the input 
+ * via .getElementById().value and compairs it to the static given login data. 
+ * If the data is not exactly what it should be, the method decrements the attemps
+ * and sends an allert to the user. After three attmeps the input fields get 
+ * disabled as well as the Button. 
+ *   
+ * @returns true, if the login data is correct and false, if it`s not
+ */
 function validateStaffLogin()
 {   
     var username = document.getElementById("staffUsername").value;
     var password = document.getElementById("staffPassword").value;
+    
     if(username == "Staff" && password == "Staff")
     {   
         return true;
